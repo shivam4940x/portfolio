22dba88eb1006dc4189ff2d4786b3b6bb75b546d
shivam singh <shivam4940x@gmail.com>
2025-06-26 00:34:15 +0530

    work section done

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -5,11 +5,12 @@ import { animate } from "animejs";
 import { useEffect, useRef } from "react";
 
 interface WorkItem {
+  name: string;
   heading: string;
   sub: string;
   techStack?: string[];
 }
-const List = ({ heading, sub, techStack }: WorkItem) => {
+const List = ({ heading, sub, techStack, name }: WorkItem) => {
   const followTheMouseMr = useRef<HTMLDivElement>(null);
   const mousePos = useRef({ x: 0, y: 0 });
   const currentPos = useRef({ x: 0, y: 0 });
@@ -55,7 +56,7 @@ const List = ({ heading, sub, techStack }: WorkItem) => {
 
   return (
     <TransitionLink
-      to={`/work/abc`}
+      to={`/work/${name}`}
       className="border-b last:border-b-0 border-border-light"
     >
       <div
@@ -63,7 +64,7 @@ const List = ({ heading, sub, techStack }: WorkItem) => {
         onMouseMove={mouseMove}
         onMouseEnter={() => animateScale(1)}
         onMouseLeave={() => animateScale(0)}
-        className="min-h-36 bg-dull-black px-12 center py-12 relative group overflow-hidden cursor-pointer"
+        className="min-h-36 bg-dull-black md:p-12 px-6 py-10 relative group overflow-hidden cursor-pointer"
       >
         <div
           ref={followTheMouseMr}
@@ -72,15 +73,16 @@ const List = ({ heading, sub, techStack }: WorkItem) => {
         >
           View
         </div>
-        <div className="grid div justify-between items-center grid-cols-5 gap-2 grayscale-100 group-hover:scale-105 group-hover:grayscale-0 duration-300">
-          <div className="flex gap-6 col-span-3">
-            <div className="font-pixel text-5xl center">
+        <div className="max-w-full flex flex-col grayscale-100 md:flex-row md:grid md:grid-cols-5 md:gap-4 items-center justify-between group-hover:scale-105 group-hover:grayscale-0 duration-300">
+          {/* Left Section */}
+          <div className="flex gap-6 col-span-3 items-center w-full md:max-w-full">
+            <div className="font-pixel md:text-5xl text-center hidden lg:block text-secondary">
               <span>&gt;</span>
             </div>
-            <div className="space-y-3">
-              <div className="relative w-max py-2 px-4">
-                <h3 className="text-xl md:text-2xl font-semibold relative z-10">
-                  {heading}
+            <div className="space-y-4 max-w-full">
+              <div className="relative md:w-max w-full max-w-full py-2 px-4">
+                <h3 className="text-xl md:text-2xl text-center md:text-left font-semibold relative z-10 break-words">
+                  <span className="text-complimentary">{heading}</span>
                 </h3>
                 <div className="absolute takeScreen">
                   <div className="div relative">
@@ -89,15 +91,19 @@ const List = ({ heading, sub, techStack }: WorkItem) => {
                   </div>
                 </div>
               </div>
-              <p className="text-sm md:text-base text-muted">{sub}</p>
+              <p className="text-sm md:text-base text-muted break-words">
+                {sub}
+              </p>
             </div>
           </div>
-          <div className="col-span-2">
-            <ul className="mt-2 flex flex-wrap gap-1">
+
+          {/* Right Section */}
+          <div className="col-span-2 mt-8 md:mt-0">
+            <ul className="flex flex-wrap gap-2 justify-end md:justify-start">
               {techStack?.map((tech, index) => (
                 <li
                   key={index}
-                  className="px-2 py-1 text-xs md:text-sm bg-muted/10 border border-border"
+                  className="px-2 py-1 text-xs md:text-sm bg-muted/10 border border-secondary/70 break-words"
                 >
                   {tech}
                 </li>
@@ -112,14 +118,14 @@ const List = ({ heading, sub, techStack }: WorkItem) => {
 const Work = () => {
   return (
     <>
-      <section className="min-h-full mt-8">
-        <div className="border-b px-8 py-2">
+      <section className="min-h-full md:mt-8 mt-1">
+        <div className="border-b md:px-8 md:py-2 pt-5.5 px-4">
           <h1 className="heading">
             <TextIn>Work</TextIn>
           </h1>
         </div>
-        <div className="mt-8">
-          <div className="px-12 py-4">
+        <div className="md:mt-8 mt-4">
+          <div className="md:px-12 md:py-4 px-6 pt-3">
             <h2 className="heading_2">
               <TextIn
                 alternative={true}
@@ -133,8 +139,8 @@ const Work = () => {
             })}
           </div>
         </div>
-        <div className="mt-8">
-          <div className="px-12 py-4">
+        <div className="md:mt-8 mt-4">
+          <div className="md:px-12 md:py-4 px-6 pt-3">
             <h2 className="heading_2">
               <TextIn
                 alternative={true}
